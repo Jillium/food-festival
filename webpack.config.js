bcfa6091e9cd0866052b46f1fe07e16930c65928
Jillium <86424914+Jillium@users.noreply.github.com>
2022-01-19 10:18:12 -0700

    more webpack and loading optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,52 @@
 const path = require("path");
 const webpack  = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 
 // webpack proerties entry, output and mode
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: 'jquery'
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an html file in the dist folder 
+        })
     ],
     mode: 'development'
 };
